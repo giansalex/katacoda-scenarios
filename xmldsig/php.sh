1b7b7ea9ba13f619570521c61651ae5b309764e4
Giancarlos Salas <giansalex@gmail.com>
2018-03-10 12:14:36 -0500

    add shebang

--- xmldsig/php.sh
+++ xmldsig/php.sh
@@ -1,1 +1,4 @@
-sudo apt-get install php && curl --silent --show-error -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+#!/bin/bash
+sudo apt-get install php
+curl --silent --show-error -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+cd root
