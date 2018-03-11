2975ac16f2ff6b507126dcf6f93655ce80b3ccba
giansalex <giansalex@gmail.com>
2018-03-11 14:53:32 -0500

    fix install sh

--- xmldsig/php.sh
+++ xmldsig/php.sh
@@ -1,1 +1,1 @@
-sudo apt-get install -y php7.0-cli && curl --silent --show-error -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer && cd root
+sudo apt-get install -y php7.0-cli && curl --silent --show-error -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
